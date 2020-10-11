5c412ef55893dfb097d266653cf86eba28c109df
Udit Jaitly <jaitlyudit@gmail.com>
2020-10-10 21:19:55 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import MyNavBar from "./components/my-navbar/mynavbar.component";
+import './App.css';
+
+const App = () =>  {
+  return (<div> 
+    <MyNavBar></MyNavBar>
+    My Portfolio
+    </div>
+    )
+  
+};
+
+export default App;
