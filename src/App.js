f6504bd1a93801d115e19b0b936e2991e3bdf1be
Udit Jaitly <jaitlyudit@gmail.com>
2020-10-11 00:20:25 -0400

    scrolling implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import MyCarousal from "./components/my-carousal/my-carousal.component.jsx"
 import MyNavBar from "./components/my-navbar/mynavbar.component";
+import MyTitle from "./components/my-title/my-title.component.jsx"
 import './App.css';
 
 const App = () =>  {
   return (<div> 
     <MyNavBar></MyNavBar>
-    My Portfolio
+    <MyCarousal></MyCarousal>
+    <MyTitle></MyTitle>
     </div>
     )
   
