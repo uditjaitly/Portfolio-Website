fdca8f91b1f2f8de16beee4f7fd9b795d5710ccc
Udit Jaitly <jaitlyudit@gmail.com>
2020-10-12 21:49:37 -0400

    Updates

---
+++ precache-manifest.c478cad809007cbcdb88072734b6cbd6.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d061ca4c0875a6fd63f9668f6298ee",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "59bcaeedb11dc60e567c",
+    "url": "/Portfolio-Website/static/css/main.7aadc4c2.chunk.css"
+  },
+  {
+    "revision": "9bdede9cb3904b698835",
+    "url": "/Portfolio-Website/static/js/2.0cf41173.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Portfolio-Website/static/js/2.0cf41173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59bcaeedb11dc60e567c",
+    "url": "/Portfolio-Website/static/js/main.b558109d.chunk.js"
+  },
+  {
+    "revision": "253420fad8cd700438d3",
+    "url": "/Portfolio-Website/static/js/runtime-main.e73aa5a6.js"
+  },
+  {
+    "revision": "3a4d476e85c4e754c198fe867afd4434",
+    "url": "/Portfolio-Website/static/media/5.3a4d476e.webp"
+  },
+  {
+    "revision": "9a747e566dbd067518a5892e6b79c8c2",
+    "url": "/Portfolio-Website/static/media/Profile.9a747e56.png"
+  },
+  {
+    "revision": "fdff4014b22f67787c176c6aa52890a7",
+    "url": "/Portfolio-Website/static/media/aws-2.fdff4014.svg"
+  },
+  {
+    "revision": "5be9bde46141498a7cfd4902b92133b1",
+    "url": "/Portfolio-Website/static/media/c.5be9bde4.svg"
+  },
+  {
+    "revision": "0a88757e27af1aac79aedd40f2ab9880",
+    "url": "/Portfolio-Website/static/media/csharp.0a88757e.png"
+  },
+  {
+    "revision": "6971ddc761cc35ba918be0a69dfbcf50",
+    "url": "/Portfolio-Website/static/media/css-5.6971ddc7.svg"
+  },
+  {
+    "revision": "a071253b062c285700dfb28a8511fcda",
+    "url": "/Portfolio-Website/static/media/customersegmentation.a071253b.png"
+  },
+  {
+    "revision": "e4517c90a2c58ee4a02d56da297b24f3",
+    "url": "/Portfolio-Website/static/media/docker-logo.e4517c90.png"
+  },
+  {
+    "revision": "096438488dba28989763c9086ba3dc47",
+    "url": "/Portfolio-Website/static/media/google-cloud-1.09643848.svg"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/Portfolio-Website/static/media/html5.f0361c5e.svg"
+  },
+  {
+    "revision": "41c391e3886f035bc6be8c230b25f5e2",
+    "url": "/Portfolio-Website/static/media/imagecaptioning.41c391e3.png"
+  },
+  {
+    "revision": "bcfad0f2c3e9c170d9e85e09a061d650",
+    "url": "/Portfolio-Website/static/media/java-4.bcfad0f2.svg"
+  },
+  {
+    "revision": "b8f420b1d589d1c72703f8ffb598dcf6",
+    "url": "/Portfolio-Website/static/media/jupyter.b8f420b1.png"
+  },
+  {
+    "revision": "6a347940f064146525be36b805414901",
+    "url": "/Portfolio-Website/static/media/kube.6a347940.png"
+  },
+  {
+    "revision": "e9de4c5331ed65d6c8b0909317cced33",
+    "url": "/Portfolio-Website/static/media/leetcode.e9de4c53.png"
+  },
+  {
+    "revision": "03a34c7c2a832dbc31a5ada2f509c41a",
+    "url": "/Portfolio-Website/static/media/logow.03a34c7c.webp"
+  },
+  {
+    "revision": "f546e5aa028365b06d2c2a08f83dda09",
+    "url": "/Portfolio-Website/static/media/microfocus.f546e5aa.png"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/Portfolio-Website/static/media/nodejs-1.a1231528.svg"
+  },
+  {
+    "revision": "7ce8647be6bfaf16bc4ac35cbedf32bf",
+    "url": "/Portfolio-Website/static/media/python-4.7ce8647b.svg"
+  },
+  {
+    "revision": "b677e8a3d3a1d71397e63a8025c3e281",
+    "url": "/Portfolio-Website/static/media/rbtimplementation.b677e8a3.png"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/Portfolio-Website/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "8bce7aef886c3872c218e0e6ef8bdccb",
+    "url": "/Portfolio-Website/static/media/slide1.8bce7aef.webp"
+  },
+  {
+    "revision": "7256f309520276be70acd97720610bc6",
+    "url": "/Portfolio-Website/static/media/slide2.7256f309.webp"
+  },
+  {
+    "revision": "8e5b87b60151049a8df9ec0bd30978f0",
+    "url": "/Portfolio-Website/static/media/slide3.8e5b87b6.webp"
+  },
+  {
+    "revision": "af968b1ef0e169695ab1f4b3a9c5b331",
+    "url": "/Portfolio-Website/static/media/smartcar.af968b1e.png"
+  },
+  {
+    "revision": "84fbe59a931a4ab62eaccadd480f13c0",
+    "url": "/Portfolio-Website/static/media/survivalgame.84fbe59a.png"
+  },
+  {
+    "revision": "1402b4895a6583b60dcb181623def9c7",
+    "url": "/Portfolio-Website/static/media/tensor.1402b489.png"
+  },
+  {
+    "revision": "73cc807b6326aabf5ddba23fb461e74d",
+    "url": "/Portfolio-Website/static/media/todoapp.73cc807b.png"
+  }
+]);
